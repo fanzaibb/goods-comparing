33a3ef127d325c9c710fc0ffbf85ccdde78bae83
Fanzai <fan11204@gmail.com>
2023-11-10 13:36:21 +0800

    build(config): add base path

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const path = require("path");
 
 const nextConfig = {
+  basePath: '/goods-comparing',
   output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+
 };
 
 module.exports = nextConfig;
